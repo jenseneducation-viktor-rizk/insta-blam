ddf08a3a049d00724960c3a85740bd4c6f71fba6
jenseneducation-viktor-rizk <55272941+jenseneducation-viktor-rizk@users.noreply.github.com>
2020-05-25 19:59:36 +0100

    cashe update version added

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,69 @@
-self.addEventListener('install',event => {
-    console.log('SW installed at: ',newDate().toLocaleTimeString());
+const filesToCache = [
+    'offline.html'
+];
+  
+const staticCacheName = 'v1';
+
+// INSTALL
+self.addEventListener('install', event => {
+    console.log('SW installed at: ', new Date().toLocaleTimeString());
+
+    self.skipWaiting()
+
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache)
+        })
+    )
+
 });
 
-self.addEventListener('activate',event => {
-    console.log('SW activated at: ',newDate().toLocaleTimeString());
-})
+// ACTIVATE
+self.addEventListener('activate', event => {
+    console.log('SW activated at: ', new Date().toLocaleTimeString());
+    
+      const cacheWhitelist = [staticCacheName];
+    
+      event.waitUntil(
+        caches.keys().then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            })
+          );
+        })
+      );
+    });
+
+
+// FETCH
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+        
+        .then(response => {
+            
+            return caches.open(staticCacheName).then(cache => {
+              cache.put(event.request.url, response.clone());
+              return response;
+            });
+          });
+  
+      }).catch(error => {
+        console.log(error)
+
+        return caches.match('offline.html')
+      })
+    );
+  });
--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 module.exports = {
   devServer: {
     https: {
-      key: fs.readFileSync("./certs/localhost-key.pem"),
-      cert: fs.readFileSync("./certs/localhost.pem")
+      key: fs.readFileSync("./certs/example.com+5-key.pem"),
+      cert: fs.readFileSync("./certs/example.com+5.pem")
     }
   }
 };
